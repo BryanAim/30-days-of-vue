27873b0bbc39ffc699cf1908998d4e16d10390d2
Isale <isale.bryan@gmail.com>
2020-07-10 10:59:08 +0300

    day 1

--- day-01/src/main.js
+++ day-01/src/main.js
@@ -1,6 +1,11 @@
 new Vue({
-  el: '#app',
-  data: {
-    greeting: 'Hello World!',
+  //options- details of the instance
+  el: '#app', //element which the instance is to be mounted, ie div where id=app
+  data: { //data that needs to be handled within the view
+    greeting: 'Hello Bryan!',
   },
 });
+
+
+// The Vue instance is the heart of a vue application and is created
+// by declaring the new Vue({}) constructor
