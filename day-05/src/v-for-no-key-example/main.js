c0fb2c92eeeb6add26b21f66d0b34477d5b8edb7
Isale <isale.bryan@gmail.com>
2020-07-14 10:33:05 +0300

    v-for js

--- day-05/src/v-for-no-key-example/main.js
+++ day-05/src/v-for-no-key-example/main.js
@@ -1,11 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    numbers: [1, 10, 100, 1000, 10000],
-  },
-  methods: {
-    shuffle() {
-      this.numbers = _.shuffle(this.numbers)
-    }
+    numbers: [1, 100, 1000, 10000]
   }
-});
+})
