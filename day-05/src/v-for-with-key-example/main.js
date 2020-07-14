140cca907e2ea52da374f6832e6881e358f7149a
Isale <isale.bryan@gmail.com>
2020-07-14 10:49:08 +0300

    with key js

--- day-05/src/v-for-with-key-example/main.js
+++ day-05/src/v-for-with-key-example/main.js
@@ -1,17 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    numbers: [
-      {id: 1, value: 1},
-      {id: 2, value: 10},
-      {id: 3, value: 100},
-      {id: 4, value: 1000},
-      {id: 5, value: 10000}
-    ],
+    numbers: [1, 2]
   },
   methods: {
     shuffle() {
       this.numbers = _.shuffle(this.numbers)
     }
-  }
-});
+  },
+})
