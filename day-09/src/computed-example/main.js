401c8b5cdf0afe9a27d7086852f9032c39961d5d
Isale <isale.bryan@gmail.com>
2020-07-18 10:35:51 +0300

    get() n set() to compute values when eithr changes

--- day-09/src/computed-example/main.js
+++ day-09/src/computed-example/main.js
@@ -6,11 +6,14 @@ new Vue({
   computed: {
     m: {
       get() {
+        // get m value
         return this.km * 1000;
       },
-      set(newValue) {
+      set (newValue) {
+        // when m changes, this function runs
         this.km = newValue/1000;
+        
       }
     }
-  }
-});
+  },
+})
