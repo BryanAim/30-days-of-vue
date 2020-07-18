fde6966e58f699e06f8d1197ecea530193761956
Isale <isale.bryan@gmail.com>
2020-07-18 10:13:48 +0300

    bug fix

--- day-09/src/watchers-example/main.js
+++ day-09/src/watchers-example/main.js
@@ -3,5 +3,15 @@ new Vue({
   data: {
     km: 0,
     m: 0
-  }
+  },
+  watch: {
+    km() {
+      // Whenever km data value changes, this function runs
+      this.m = this.km ? this.km * 1000 : 0;
+    },
+    m() {
+      // Whenever m data value changes, this function runs
+      this.km = this.m ? this.m / 1000 : 0;
+    }
+  },
 })
