1624184249c591a2814daa2c78886fd37eede12e
Isale <isale.bryan@gmail.com>
2020-07-18 09:49:22 +0300

    initialize watchers js

--- day-09/src/watchers-example/main.js
+++ day-09/src/watchers-example/main.js
@@ -1,15 +1,7 @@
 new Vue({
-  el: '#app',
+  el:"#app",
   data: {
     km: 0,
     m: 0
-  },
-  watch: {
-    km() {
-      this.m = this.km ? this.km * 1000 : 0;
-    },
-    m() {
-      this.km = this.m ? this.m / 1000 : 0;
-    }
   }
-});
+})
