2f766a0efe8402bcb0f349d675d4957423a7020c
Isale <isale.bryan@gmail.com>
2020-07-19 12:12:49 +0300

    use fetch on created() to get data

--- day-10/src/created-example/main.js
+++ day-10/src/created-example/main.js
@@ -7,5 +7,17 @@ new Vue({
       name: '',
       catchPhrase: ''
     }
-  }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response)=> {
+      //use response to update data
+      const data = response.data;
+      const randomUser = response.data[Math.floor(Math.random() * data.length)];
+
+      this.name = randomUser.name;
+      this.email = randomUser.email;
+      this.company.name = randomUser.company.name;
+      this.company.catchPhrase = randomUser.company.catchPhrase
+    })
+  },
 })
