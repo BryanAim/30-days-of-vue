27a6959bb89f9e7c53292c5122e5fffc0536d518
Isale <isale.bryan@gmail.com>
2020-07-19 11:55:57 +0300

    created() base JS

--- day-10/src/created-example/main.js
+++ day-10/src/created-example/main.js
@@ -1,27 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Loading...',
-    email: 'Loading...',
+    name: '',
+    email: '',
     company: {
-      name: 'Loading...',
-      catchPhrase: 'Loading...',
+      name: '',
+      catchPhrase: ''
     }
-  },
-  created() {
-    axios.get(
-      'https://jsonplaceholder.typicode.com/users'
-    ).then((response) => {
-        const data = response.data;
-        const randomUser = response.data[
-          Math.floor(Math.random() * data.length)
-        ];
- 
-        this.name = randomUser.name;
-        this.email = randomUser.email;
-        this.company.name = randomUser.company.name;
-        this.company.catchPhrase = 
-          randomUser.company.catchPhrase;
-      });
-  },
-});
+  }
+})
