2b88b7831a4de723a6c3f9c1babeea3b56692c5e
Isale <isale.bryan@gmail.com>
2020-07-21 09:59:36 +0300

    nested components base JS

--- day-12/src/nested-components-example/main.js
+++ day-12/src/nested-components-example/main.js
@@ -1,81 +1,84 @@
 const tweets = [
   {
     id: 1,
-    name: 'James',
-    handle: '@jokerjames',
+    name: 'Isale',
+    handle: '@isalebryan',
     img: './images/james.png',
-    tweet: "If you don't succeed, dust yourself off and try again.",
-    likes: 10,
+    tweet: "If you don't succeed, dust yourself off...",
+    likes: 10
   },
-  { 
+  {
     id: 2,
     name: 'Fatima',
     handle: '@fantasticfatima',
     img: './images/fatima.png',
-    tweet: 'Better late than never but never late is better.',
-    likes: 12,
+    tweet: 'Better late than never but never late...',
+    likes: 12
   },
   {
     id: 3,
     name: 'Xin',
     handle: '@xeroxin',
     img: './images/xin.png',
-    tweet: 'Beauty in the struggle, ugliness in the success.',
-    likes: 18,
+    tweet: 'Beauty in the struggle...',
+    likes: 18
   }
+  
 ];
 
-Vue.component('tweet-content', {
-  template: `
-    <div class="media-content">
-      <div class="content">
-        <p>
-          <strong>{{tweet.name}}</strong>
-          <small>{{tweet.handle}}</small>
-          <br>
-          {{tweet.tweet}}
-        </p>
-      </div>
-      <div class="level-left">
-        <a class="level-item">
-          <span class="icon is-small">
-            <i class="fas fa-heart"></i>
-          </span>
-          <span class="likes">{{tweet.likes}}</span>
-        </a>
-      </div>
-    </div>
-  `,
-  props: ['tweet']
-});
 
+// The Vue.component() constructor registers a component globally in an application.
 Vue.component('tweet-component', {
-  template: `   
-    <div class="tweet">
-      <div class="box">
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img :src="tweet.img">
-            </figure>
+  //options
+  template:`
+  <div class="tweet">
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img :src="tweet.img" >
+          </figure>
+        </div>
+        <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>{{ tweet.name}}</strong>
+                <small>{{ tweet.email }}</small>
+                <br>
+                {{ tweet.tweet}}
+              </p>
+            </div>
+            <div class="level-left">
+              <a class="level-item">
+                <span class="icon is-small">
+                  <i class="fas fa-heart"></i>
+                </span>
+                <span class="likes">
+                  {{ tweet.likes }}
+                </span>
+              </a>
+            </div>
           </div>
-          <tweet-content :tweet="tweet"></tweet-content>
-        </article>
-      </div>
-      <div class="control has-icons-left">
+      </article>
+    </div>
+    <div class="control has-icons-left">
         <input class="input is-small"
           placeholder="Tweet your reply..." />
         <span class="icon is-small is-left">
           <i class="fas fa-envelope"></i>
         </span>
       </div>
-    </div> 
-  `,
-  props: ['tweet']
-});
+  </div>`,
+  props: {
+    tweet: {
+      type: Object,
+      required: true
+    }
+  }
+})
 
 new Vue({
-  el: '#app',
+  el:'#app',
   data: {
     tweets
   }
