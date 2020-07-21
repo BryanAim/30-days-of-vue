c7aa0534855bc25dfe88abee223fe2eac6445095
Isale <isale.bryan@gmail.com>
2020-07-21 09:41:13 +0300

    props base JS

--- day-12/src/props-example/main.js
+++ day-12/src/props-example/main.js
@@ -1,47 +1,51 @@
 const tweets = [
   {
     id: 1,
-    name: 'James',
-    handle: '@jokerjames',
+    name: 'Isale',
+    handle: '@isalebryan',
     img: './images/james.png',
-    tweet: "If you don't succeed, dust yourself off and try again.",
-    likes: 10,
+    tweet: "If you don't succeed, dust yourself off...",
+    likes: 10
   },
-  { 
+  {
     id: 2,
     name: 'Fatima',
     handle: '@fantasticfatima',
     img: './images/fatima.png',
-    tweet: 'Better late than never but never late is better.',
-    likes: 12,
+    tweet: 'Better late than never but never late...',
+    likes: 12
   },
   {
     id: 3,
     name: 'Xin',
     handle: '@xeroxin',
     img: './images/xin.png',
-    tweet: 'Beauty in the struggle, ugliness in the success.',
-    likes: 18,
+    tweet: 'Beauty in the struggle...',
+    likes: 18
   }
+  
 ];
 
+
+// The Vue.component() constructor registers a component globally in an application.
 Vue.component('tweet-component', {
-  template: `   
-    <div class="tweet">
-      <div class="box">
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img :src="tweet.img">
-            </figure>
-          </div>
-          <div class="media-content">
+  //options
+  template:`
+  <div class="tweet">
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img src="./images/james.png" >
+          </figure>
+        </div>
+        <div class="media-content">
             <div class="content">
               <p>
-                <strong>{{tweet.name}}</strong>
-                <small>{{tweet.handle}}</small>
+                <strong>Isale</strong>
+                <small>@isalebryan</small>
                 <br>
-                {{tweet.tweet}}
+                If you don't succeed, dust yourself off...
               </p>
             </div>
             <div class="level-left">
@@ -50,27 +54,25 @@ Vue.component('tweet-component', {
                   <i class="fas fa-heart"></i>
                 </span>
                 <span class="likes">
-                  {{tweet.likes}}
+                  10
                 </span>
               </a>
             </div>
           </div>
-        </article>
-      </div>
-      <div class="control has-icons-left">
+      </article>
+    </div>
+    <div class="control has-icons-left">
         <input class="input is-small"
           placeholder="Tweet your reply..." />
         <span class="icon is-small is-left">
           <i class="fas fa-envelope"></i>
         </span>
       </div>
-    </div>
-  `,
-  props: ['tweet']
-});
+  </div>`
+})
 
 new Vue({
-  el: '#app',
+  el:'#app',
   data: {
     tweets
   }
