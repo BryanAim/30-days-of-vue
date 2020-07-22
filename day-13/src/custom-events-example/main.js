72c497b8b2f73c862e3823697c063da4362da6cd
Isale <isale.bryan@gmail.com>
2020-07-22 12:30:56 +0300

    custom events base html and JS

--- day-13/src/custom-events-example/main.js
+++ day-13/src/custom-events-example/main.js
@@ -1,39 +1,42 @@
 const tweets = [
   {
     id: 1,
-    name: 'James',
-    handle: '@jokerjames',
+    name: 'Isale',
+    handle: '@isalebryan',
     img: './images/james.png',
-    tweet: "If you don't succeed, dust yourself off and try again.",
-    likes: 10,
+    tweet: "If you don't succeed, dust yourself off...",
+    likes: 10
   },
-  { 
+  {
     id: 2,
     name: 'Fatima',
     handle: '@fantasticfatima',
     img: './images/fatima.png',
-    tweet: 'Better late than never but never late is better.',
-    likes: 12,
+    tweet: 'Better late than never but never late...',
+    likes: 12
   },
   {
     id: 3,
     name: 'Xin',
     handle: '@xeroxin',
     img: './images/xin.png',
-    tweet: 'Beauty in the struggle, ugliness in the success.',
-    likes: 18,
+    tweet: 'Beauty in the struggle...',
+    likes: 18
   }
+  
 ];
 
+
+// The Vue.component() constructor registers a component globally in an application.
 Vue.component('tweet-content', {
   template: `
     <div class="media-content">
       <div class="content">
         <p>
-          <strong>{{tweet.name}}</strong>
-          <small>{{tweet.handle}}</small>
+          <strong> {{ tweet.name }}</strong>
+          <small> {{ tweet.handle }}</small>
           <br>
-          {{tweet.tweet}}
+          {{ tweet.tweet}}
         </p>
       </div>
       <div class="level-left">
@@ -41,64 +44,47 @@ Vue.component('tweet-content', {
           <span class="icon is-small">
             <i class="fas fa-heart"></i>
           </span>
-          <span class="likes">{{tweet.likes}}</span>
+          <span class="likes"> {{ tweet.likes }}</span>
         </a>
-        <a class="level-item"
-          @click="$emit('add', tweet)">
-          <span class="icon is-small">
-            <i class="fas fa-plus"></i>
-          </span>
-        </a>
-      </div>
+      </div>  
     </div>
   `,
   props: ['tweet']
 });
 
 Vue.component('tweet-component', {
-  template: `
-    <div class="tweet">
-      <div class="box">
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img :src="tweet.img">
-            </figure>
-          </div>
-          <tweet-content :tweet="tweet"
-            @add="$emit('add', $event)">
-          </tweet-content>
-        </article>
-      </div>
-      <div class="control has-icons-left">
+  //options
+  template:`
+  <div class="tweet">
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img :src="tweet.img" >
+          </figure>
+        </div>
+        <tweet-content :tweet="tweet"></tweet-content>
+      </article>
+    </div>
+    <div class="control has-icons-left">
         <input class="input is-small"
           placeholder="Tweet your reply..." />
         <span class="icon is-small is-left">
           <i class="fas fa-envelope"></i>
         </span>
       </div>
-    </div>`,
-    props: ['tweet']
-});
+  </div>`,
+  props: {
+    tweet: {
+      type: Object,
+      required: true
+    }
+  }
+})
 
 new Vue({
-  el: '#app',
+  el:'#app',
   data: {
     tweets
-  },
-  methods: {
-    addTweetMessage(tweet) {
-      let newTweet = {};
-      let lastTweetObjectID =
-        this.tweets[this.tweets.length - 1].id;
-
-      // shallow copy tweet object
-      newTweet = Object.assign({}, tweet);
-      
-      // set tweet id to be one greater than last tweet
-      newTweet.id = lastTweetObjectID + 1; 
- 
-      this.tweets.push(newTweet);
-    }
   }
 });
